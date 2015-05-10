18340f2100b17d0260c01f74055bf810fbfc2491
Searingfang <tmatsuz@calstatela.edu>
2015-05-09 17:02:49 -0700

    commit

--- MarsEnvironmentSimulator/src/map/Program.java
+++ MarsEnvironmentSimulator/src/map/Program.java
@@ -76,7 +76,7 @@ public class Program {
 	private static void clearGL(){
 		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
 		//Uncomment this if you want to see the map as a wireframe!
-		GL11.glPolygonMode(GL11.GL_FRONT_AND_BACK, GL11.GL_LINE);
+		//GL11.glPolygonMode(GL11.GL_FRONT_AND_BACK, GL11.GL_LINE);
 		GL11.glLoadIdentity();
 	}
 }
